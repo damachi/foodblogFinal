8c092a86745066f8946286a41408ea051de6b885
Francis Damachi <damachi49@gmail.com>
2023-08-09 15:54:53 +0200

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,12 +7,33 @@ import Image from 'next/image'
 
 export default function AboutMe() {
 
-
     return (
-        <div className='w-full h-screen bg-backgroundimage bg-no-repeat bg-cover bg-center'>
-            hello
+        <div className='w-full bg-backgroundimage bg-no-repeat bg-cover bg-center flex justify-center items-center p-2'>
+            <div className='w-2/3  bg-white rounded-lg bg-opacity-70 text-black p-4 overflow-auto text-center'>
+
+
+                <span className='text-2xl'>👋 Hi there!</span> <br></br>
+
+                Welcome to my little corner of the internet! I'm <span className='font-bold'>FRANCIS DAMACHI</span>, a sports enthusiast by day, culinary maestro by night, and a dedicated recipe follower in between.<br></br>
+
+                When I'm not cheering on my favorite teams or working up a sweat in the local gym, you'll find me in my most cherished place - the kitchen! My playground, my laboratory, my sanctuary - this is where I chase the thrill of victory, the same way I do in sports, but with flavors, aromas and textures instead of balls and rackets. 🍳 <br></br>
+
+                My food journey has been an exciting rollercoaster ride, chasing flavors from around the globe, right here from my humble kitchen. I'm all about authentic cuisine, made accessible. From the perfect Italian gnocchi that’s as fluffy as clouds, to the subtly spiced Indian biryani that’s an orchestra of flavors – my mission is to make these culinary wonders doable for everyone. 🍝🍛 <br></br>
+
+                But the end game? Well, I'm just warming up! One day, I aim to master the most complex, intricate recipes out there. Michelin-star level? Why not! It's all part of the game, and I’m in it for the long haul. 🌟 <br></br>
+
+                My current secret weapon is simplicity. I'm a staunch advocate of 'easy-to-follow recipes.' I firmly believe that the beauty of cooking lies not in juggling a hundred ingredients at once, but in finding the essence of a dish and bringing it to life in the simplest way possible. 🎯 <br></br>
+
+                But enough about me! This space is about you, my fellow food adventurers! So, put on your chef's hat, dust off that apron, and join me as we turn everyday ingredients into extraordinary meals, and as we march towards conquering the culinary Everest. 🥘
+
+                Buckle up and hold on to your spatulas - it's going to be a deliciously challenging ride! 🚀 <br></br>
+
+                Looking forward to cooking up a storm with you, <br></br>
+
+                FRANCIS DAMACHI
+
+            </div>
         </div>
-        
     )
 }
 

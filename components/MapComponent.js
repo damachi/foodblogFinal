9525b3c51b0ccaf62512e6563da7d7232cf47b73
Francis Damachi <damachi49@gmail.com>
2023-08-14 16:38:24 +0200

    imprvoed map again

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -46,10 +46,12 @@ const WorldMap = ({ data }) => {
     const MIN_WIDTH = 320
     const MAX_WIDTH = 2560
 
-    const projection = d3.geoNaturalEarth1()
-      .scale(width / getScaleFactor(width, MIN_WIDTH, MAX_WIDTH) / Math.PI)
-      .translate([width / 2, height / 2]);
-
+    const projection = d3.geoConicConformal()
+    .center([15, 30])  // Roughly centers between Europe and Africa
+    .rotate([-15, 0])  // Rotates to emphasize the region
+    .parallels([20, 40])  // Parallels that encompass Europe and Africa
+    .scale(width / getScaleFactor(width, MIN_WIDTH, MAX_WIDTH))  // Adjust scale based on your preference
+    .translate([width / 2, height / 2]);
     // // console.log(width)
 
     svg.selectAll("*").remove();
@@ -74,11 +76,30 @@ function getScale(width) {
 }
 
 const getScaleFactor = (width, minWidth, maxWidth) => {
-  const minScale = 1.5;
-  const maxScale = 3.0;
-  if (width <= minWidth) return minScale;
-  if (width >= maxWidth) return maxScale;
-  return minScale + (width - minWidth) / (maxWidth - minWidth) * (maxScale - minScale);
+  // const minScale = 3.0;
+  // const maxScale = 8.0;
+  // if (width <= minWidth) return minScale;
+  // if (width >= maxWidth) return maxScale;
+  // return minScale + (width - minWidth) / (maxWidth - minWidth) * (maxScale - minScale);
+
+  if(width >= 1440){
+
+    return 8.5
+  }else if(width >= 1024){
+    return 8.3
+  }else if(width >= 768){
+    return 5.4
+  }else if(width >= 425){
+    return 3.0
+  }else if(width >= 375){
+    return 2.6
+  }else if(width >= 320){
+    return 2.3
+  }else{
+    return 1.0
+  }
+
+
 };
 
 

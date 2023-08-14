7546665467d7e058839f80cbca3b8b748a6a1d07
Francis Damachi <damachi49@gmail.com>
2023-08-14 16:04:30 +0200

    improved map

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,66 +1,85 @@
 "use client"
+
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import geoAzimuthalEqualArea from 'd3-geo-projection'
 
 const WorldMap = ({ data }) => {
-  const ref = useRef(); // Reference to the SVG element
+  const ref = useRef();
 
   useEffect(() => {
     const svg = d3.select(ref.current);
 
-    // Function to handle the actual redrawing
+    const zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', zoomed);
+
+    svg.call(zoom);
+
+    function zoomed(event) {
+      const { transform } = event;
+      svg.select('g').attr('transform', transform);
+    }
+
     const handleResize = () => {
       const width = svg.node().getBoundingClientRect().width;
       const height = svg.node().getBoundingClientRect().height;
       renderMap(svg, width, height);
     };
 
-    // Set up ResizeObserver
     const resizeObserver = new ResizeObserver(handleResize);
     resizeObserver.observe(ref.current);
 
-    // Set up resize event listener
     window.addEventListener('resize', handleResize);
 
-    // Initial draw
     handleResize();
 
     return () => {
-      // Cleanup
       resizeObserver.disconnect();
       window.removeEventListener('resize', handleResize);
     };
-  }, [data]); // Assuming you want to re-run this effect if data changes.
+  }, [data]);
 
   const renderMap = (svg, width, height) => {
-    // Calculate dynamic scale based on SVG dimensions.
-    const scaleFactor = 1;  // Adjust this for desired default scaling
-    const scale = (width / 2.5) * scaleFactor;  // 2.5 is an arbitrary factor for adjusting initial scale
+    // const scaleFactor = 1;
+    // const scale = (width / 2.5) * scaleFactor;
 
-    // const projection = d3.geoCylindricalStereographic()
-    
-      // .scale(scale)
-      const projection = d3.geoAzimuthalEqualArea()
+    const MIN_WIDTH = 320
+    const MAX_WIDTH = 2560
+
+    const projection = d3.geoNaturalEarth1()
+      .scale(width / getScaleFactor(width, MIN_WIDTH, MAX_WIDTH) / Math.PI)
       .translate([width / 2, height / 2]);
+
+    // // console.log(width)
 
     svg.selectAll("*").remove();
 
-    // Draw the map
-    svg.append("g")
-      .selectAll("path")
+    const g = svg.append("g");
+    g.selectAll("path")
       .data(data.features)
       .join("path")
       .attr("fill", "black")
       .attr("d", d3.geoPath().projection(projection))
       .style("stroke", "#fff");
-};
-
-  
+  };
 
   return (
-    <svg ref={ref} style={{ width: "100%", height: "100%" }} />
+    <svg ref={ref} style={{ width: "100%", height: "100%", overflow: "hidden" }} />
+    // <svg ref={ref} style={{ width: "425px", height: "522px", overflow: "hidden" }} />
   );
 };
+
+function getScale(width) {
+  return 1.5
+}
+
+const getScaleFactor = (width, minWidth, maxWidth) => {
+  const minScale = 1.5;
+  const maxScale = 3.0;
+  if (width <= minWidth) return minScale;
+  if (width >= maxWidth) return maxScale;
+  return minScale + (width - minWidth) / (maxWidth - minWidth) * (maxScale - minScale);
+};
+
 
 export default WorldMap;

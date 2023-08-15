469bb7a85226cc59d7c6f398e5358721f69ae6e4
Francis Damachi <damachi49@gmail.com>
2023-08-15 15:27:20 +0200

    imporved responsive

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -8,32 +8,32 @@ import { BiSolidTimer } from 'react-icons/bi'
 export default function RecipeCard() {
   return (
 
-    <div className='flex w-[255px] h-full bg-white flex-shrink-0 md:w-[240px] lg:w-[360px] border border-solid rounded-sm'>
-      <div>
+    <div className='flex flex-col w-[255px] h-full bg-white flex-shrink-0 md:w-[240px] lg:w-[360px] border border-solid rounded-sm'>
 
-        <Image
-          objectFit='cover'
-          boxSize='100%'
-          src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-          alt='Caffe Latte'
-        />
 
-      </div>
-
-      <div className='w-1/2 ps-1 flex flex-col justify-center'>
-        <div className='text-sm font-bold'>
+      <div className='w-full h-full flex flex-col justify-center'>
+        <div className='text-base font-bold'>
           Jollof Rice
+        </div>
+
+        <div className='flex-grow overflow-auto'>
+          <Image
+            objectFit='cover'
+            boxSize='100%'
+            src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+            alt='Caffe Latte'
+          />
 
         </div>
 
-        <div className='hidden lg:block text-xs'>
+        {/* <div className='hidden lg:block text-xs'>
           is a spicy West African dish with a distinctive reddish-orange hue.
-        </div>
+        </div> */}
         <Divider />
-        <div className='text-xs'>
+        <div className='text-xs flex justify-between h-11 items-center ps-1 pe-1'>
           <div><Icon as={GiLevelEndFlag} />Beginner</div>
-          <div><Icon as={BiSolidTimer} />Prep: 20m</div>
-          <div><Icon as={BiSolidTimer} />Cook: 30m</div>
+          <div><Icon as={BiSolidTimer} />Prep:20min</div>
+          <div><Icon as={BiSolidTimer} />Cook:30min</div>
 
         </div>
       </div>

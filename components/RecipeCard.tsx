43db841e0a3d0a27709fd7f128d4e05e9ac12492
Francis Damachi <damachi49@gmail.com>
2023-08-15 17:46:14 +0200

    fix bug routing killing styles

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -4,45 +4,47 @@ import { Icon } from '@chakra-ui/react'
 import { GiLevelEndFlag } from 'react-icons/gi'
 import { BiSolidTimer } from 'react-icons/bi'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 
 
 export default function RecipeCard() {
+  const router = useRouter()
   return (
 
     <div className='flex flex-col w-[255px] h-full bg-white flex-shrink-0 border border-solid rounded-sm'>
 
-    <Link href={`/recipe/`}>
 
-      <div className='w-full h-full flex flex-col justify-center cursor-pointer'>
-        <div className='text-base font-bold'>
-          Jollof Rices
-        </div>
 
-        <div className='flex-grow overflow-auto'>
-          <Image
-            objectFit='cover'
-            boxSize='100%'
-            src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-            alt='Caffe Latte'
-          />
+      <div className='w-full h-full flex flex-col justify-center cursor-pointer' onClick={() => router.push('/recipe')}>
+        
+          <div className='text-base font-bold'>
+            Jollof Rices
+          </div>
 
-        </div>
+          <div className='flex-grow overflow-auto'>
+            <Image
+              objectFit='cover'
+              boxSize='100%'
+              src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+              alt='Caffe Latte'
+            />
 
-        {/* <div className='hidden lg:block text-xs'>
+          </div>
+
+          {/* <div className='hidden lg:block text-xs'>
           is a spicy West African dish with a distinctive reddish-orange hue.
         </div> */}
-        <Divider />
-        <div className='text-xs flex justify-between h-11 items-center ps-1 pe-1'>
-          <div><Icon as={GiLevelEndFlag} />Beginner</div>
-          <Divider orientation='vertical' />
-          <div><Icon as={BiSolidTimer} />Prep:20min</div>
-          <Divider orientation='vertical' />
-          <div><Icon as={BiSolidTimer} />Cook:30min</div>
+          <Divider />
+          <div className='text-xs flex justify-between h-11 items-center ps-1 pe-1'>
+            <div><Icon as={GiLevelEndFlag} />Beginner</div>
+            <Divider orientation='vertical' />
+            <div><Icon as={BiSolidTimer} />Prep:20min</div>
+            <Divider orientation='vertical' />
+            <div><Icon as={BiSolidTimer} />Cook:30min</div>
 
-        </div>
+          </div>
       </div>
-      </Link>
     </div>
   )
 }
